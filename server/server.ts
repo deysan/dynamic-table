deb84de3088b5adb853cfc5351e793fa32cc63fd
deysan <ideysan@gmail.com>
2022-09-09 09:51:57 +0300

    Fix mime types

--- server/server.ts
+++ server/server.ts
@@ -5,19 +5,36 @@ import fs from 'fs/promises';
 import path from 'path';
 import { renderToString } from 'react-dom/server';
 
+type Mime = {
+  [key: string]: string;
+};
+
+const mimeTypes: Mime = {
+  html: 'text/html; charset=UTF-8',
+  js: 'application/javascript; charset=UTF-8',
+  css: 'text/css',
+  svg: 'image/svg+xml',
+};
+
 const server = createServer(async (req, res) => {
-  const html = await fs.readFile(path.resolve('./dist/index.html'), 'utf-8');
+  const { url } = req;
+  if (req.method === 'GET' && url) {
+    const fileExt = path.extname(url).substring(1);
+    const contentType = mimeTypes[fileExt] || mimeTypes.html;
 
-  const content = html.replace(
-    '<!--app-html-->',
-    renderToString(createElement(App)),
-  );
+    const html = await fs.readFile(path.resolve('./dist/index.html'), 'utf-8');
 
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-  });
+    const content = html.replace(
+      '<!--app-html-->',
+      renderToString(createElement(App)),
+    );
 
-  res.end(content);
+    res.writeHead(200, {
+      'Content-Type': contentType,
+    });
+
+    res.end(content);
+  }
 });
 
 server.listen(9000, () => {
