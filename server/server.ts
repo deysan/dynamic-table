09bd20854c6cbd2feeea7d19f1cf9f2bc69e81d9
deysan <ideysan@gmail.com>
2022-09-09 13:15:07 +0300

    Update server

--- server/server.ts
+++ server/server.ts
@@ -1,39 +1,59 @@
 import App from '../src/App';
 import { createElement } from 'react';
 import { createServer } from 'http';
-import fs from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 import { renderToString } from 'react-dom/server';
 
-type Mime = {
+interface Mime {
   [key: string]: string;
-};
+}
 
-const mimeTypes: Mime = {
-  html: 'text/html; charset=UTF-8',
-  js: 'application/javascript; charset=UTF-8',
-  css: 'text/css',
-  svg: 'image/svg+xml',
-};
+const server = createServer(function (req, res) {
+  let filePath = req.url;
 
-const server = createServer(async (req, res) => {
-  const { url } = req;
-  if (req.method === 'GET' && url) {
-    const fileExt = path.extname(url).substring(1);
-    const contentType = mimeTypes[fileExt] || mimeTypes.html;
+  if (filePath == '/') {
+    filePath = 'dist/index.html';
+  } else {
+    filePath = 'dist' + req.url;
+  }
 
-    const html = await fs.readFile(path.resolve('./dist/index.html'), 'utf-8');
+  if (filePath) {
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes: Mime = {
+      '.js': 'application/javascript',
+      '.css': 'text/css',
+      '.svg': 'image/svg+xml',
+    };
 
-    const content = html.replace(
-      '<!--app-html-->',
-      renderToString(createElement(App)),
-    );
+    const contentType = mimeTypes[extname] || 'text/html';
 
-    res.writeHead(200, {
-      'Content-Type': contentType,
+    fs.readFile(filePath, function (error, content) {
+      if (error) {
+        res.writeHead(500);
+        res.end(
+          'Sorry, check with the site admin for error: ' + error.code + ' ..\n',
+        );
+      } else if (contentType === 'text/html') {
+        const html = fs.readFileSync(
+          path.resolve('./dist/index.html'),
+          'utf-8',
+        );
+        const appHtml = html.replace(
+          '<!--app-html-->',
+          renderToString(createElement(App)),
+        );
+
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+        });
+
+        res.end(appHtml, 'utf-8');
+      } else {
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content, 'utf-8');
+      }
     });
-
-    res.end(content);
   }
 });
 
